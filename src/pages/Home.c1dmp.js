8f2fb336ac9ce1fb8e414928a3005fd15b2c4f8a
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2026-05-29 02:17:15 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,9 @@
+// Guia de API: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	$w($w('#btnProjeto').onClick()) => {
+
+		console.log("Botão funcionando");
+	});
+});
