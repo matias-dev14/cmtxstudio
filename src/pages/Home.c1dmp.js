ef621c405ce74e563bf9465f8d427f7530b84381
matias-dev14 <matias.dev.14@gmail.com>
2026-05-29 21:32:18 -0300

    Atualização da página Home

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -4,6 +4,22 @@ $w.onReady(function () {
 
 	$w($w('#btnProjeto').onClick()) => {
 
-		console.log("Botão funcionando");
-	});
+		console.log("CMTX Studio carregando...");
+
+		$w('#btnProjeto').onClick(() => {
+
+			console.log("Novo projeto");
+		
+});
+
+$w('#btnProjeto').onMouseIn(() => {
+
+	$w('#btnProjeto').style.backgroundColor = "#0052cc";
+
+});
+
+$w('#btnProjeto').onMouseOut(() => {
+
+	$w('#btnProjeto').style.backgroundColor = "#0066FF";
+
 });
